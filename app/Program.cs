18154a21d8a7ddec5a778ffabb4cb2be24cca36d
dmorton714 <danny.morton714@gmail.com>
2024-03-23 09:25:11 -0400

    It works.. I think

--- app/Program.cs
+++ app/Program.cs
@@ -1,19 +1,59 @@
-﻿class Product
+﻿using System;
+using System.Text.Json;
+
+class Program
 {
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public int Qty { get; set; }
-    public string Description { get; set; }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Please follow the steps to enter items!");
+        
+        Console.WriteLine("Press 1 to add a product");
+        Console.WriteLine("Type 'exit' to quit");
+
+        
+        while (true)
+        {
+            string userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "exit")
+                break;
+
+            if (userInput == "1")
+            {
+                Console.WriteLine("Enter product details:");
+
+                CatFood catFood = new CatFood();
+
+                Console.WriteLine("Enter weight in pounds:");
+                catFood.WeightPounds = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Is it kitten food? (true/false):");
+                catFood.KittenFood = bool.Parse(Console.ReadLine());
+
+                Console.WriteLine("Product added:");
+                Console.WriteLine(JsonSerializer.Serialize(catFood));
+            }
+            Console.WriteLine("Press 1 to add another product or type 'exit' to quit");
+        }
+    }
+}
+
+class Product
+{
+    string Name { get; set; }
+    decimal Price { get; set; }
+    int Qty { get; set; }
+    string Description { get; set; }
 }
 
 class CatFood
 {
-    public double WeightPounds { get; set; }
-    public bool KittenFood { get; set; }
+    public double WeightPounds { get; set; } 
+    public bool KittenFood { get; set; } 
 }
 
 class DogLeash
 {
-    public int LengthInches { get; set; }
-    public string Material { get; set; }
+    int LengthInches { get; set; }
+    string Material { get; set; }
 }
